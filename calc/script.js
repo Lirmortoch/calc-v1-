daf30daad9e11ac862da0a93418aaa25c16b53ad
Lirmortoch <dima2111323@gmail.com>
2024-09-24 14:17:30 +0300

    Добавил переключатель, сделал смену темы  по нажатию

---
+++ calc/script.js
@@ -0,0 +1,15 @@
+'use strict'
+const body = document.querySelector('body');
+const themeSwitcher = document.querySelector('.header__theme-switcher');
+
+themeSwitcher.addEventListener('click', (event) => {
+    body.classList.toggle('light-theme');
+})
+
+class Calculator { 
+    
+    
+    init() {
+
+    }
+}
