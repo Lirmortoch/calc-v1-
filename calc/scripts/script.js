bd7afd149f723d1bb57a0dafc03e5a7bfcb72ff7
Lirmortoch <dima2111323@gmail.com>
2024-10-31 19:12:44 +0300

    Небольшие измениния

--- calc/scripts/script.js
+++ calc/scripts/script.js
@@ -1,6 +1,28 @@
 'use strict'
 
+import math from '/node_modules/mathjs';
+
 const body = document.querySelector('body');
+
+/* Main functionality */
+const input = body.querySelector('.input');
+
+const inputPrev = input.firstElementChild;
+const inputMainPart = input.lastElementChild;
+
+const buttons = body.querySelector('.action-buttons');
+
+const calcMode = 'standard';
+const buttonsMainOps = ['%', 'CE', 'C', '⌫', '⅟x', 'x²', '²√x', '÷', ''];
+const scientificButtons = [];
+const scientificTopButtons = ['Trigonometry', 'Functions'];
+
+const history = [];
+const memory = [];
+
+function initCalc() {
+
+}
 
 /* Theme switch */
 const themeSwitcherIcons = ['<circle cx="12" cy="12" r="5"/><line x1="12" x2="12" y1="1" y2="3"/><line x1="12" x2="12" y1="21" y2="23"/><line x1="4.22" x2="5.64" y1="4.22" y2="5.64"/><line x1="18.36" x2="19.78" y1="18.36" y2="19.78"/><line x1="1" x2="3" y1="12" y2="12"/><line x1="21" x2="23" y1="12" y2="12"/><line x1="4.22" x2="5.64" y1="19.78" y2="18.36"/><line x1="18.36" x2="19.78" y1="5.64" y2="4.22"/>', '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>'];
@@ -53,8 +75,6 @@ themeSwitcher.addEventListener('click', switchTheme);
 /* History switch */
 const historyBlock = document.querySelector('.history-section');
 const historyActivePart = historyBlock.querySelector('.active-part');
-const history = [];
-const memory = [];
 
 let currentHistoryMode = localStorage.getItem('history-mode');
 
@@ -80,70 +100,3 @@ function turnOnHistoryMode(currentMode) {
 if (currentHistoryMode !== null) turnOnHistoryMode(currentHistoryMode);
 
 historyBlock.addEventListener('click', switchHistoryMode);
-
-
-// function switchTheme(switchToLight, switchToMain, currentMode) {
-//     currentMode !== 'light' ? switchToLight() : switchToMain();
-// }
-
-// class Calculator { 
-//     #body = document.querySelector('body');
-
-//     #themeSwitcherIcons = [
-//         '<circle cx="12" cy="12" r="5"/><line x1="12" x2="12" y1="1" y2="3"/><line x1="12" x2="12" y1="21" y2="23"/><line x1="4.22" x2="5.64" y1="4.22" y2="5.64"/><line x1="18.36" x2="19.78" y1="18.36" y2="19.78"/><line x1="1" x2="3" y1="12" y2="12"/><line x1="21" x2="23" y1="12" y2="12"/><line x1="4.22" x2="5.64" y1="19.78" y2="18.36"/><line x1="18.36" x2="19.78" y1="5.64" y2="4.22"/>', 
-//         '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>'
-//     ];
-//     #themeSwitcher = document.querySelector('.header__theme-switcher');
-//     #themeSwitcherIcon = document.getElementById('switcher-mask');
-//     #themeSwitcherGradient = document.getElementById('switcher-gradient');
-    
-//     #currentTheme = localStorage.getItem('theme');
-
-//     #deactivateBtnForTime() {
-//         this.#themeSwitcher.setAttribute("disabled", "");
-    
-//         setTimeout(() => {
-//             this.#themeSwitcher.removeAttribute("disabled", "");
-//         }, '2000');
-//     }
-    
-//     #switchToLightMode() {
-//         this.#body.classList.add('light-theme');
-//         localStorage.setItem('theme', 'light');
-//         this.#themeSwitcherIcon.innerHTML = this.#themeSwitcherIcons[1];
-//         this.#themeSwitcherGradient.style.fill = 'url(#linear-gradient-2)';
-//         this.#themeSwitcherGradient.style.stroke = 'url(#linear-gradient-2)';
-    
-//         this.#deactivateBtnForTime();
-//     }
-    
-//     #switchToMainMode() {
-//         this.#body.classList.remove('light-theme');
-//         localStorage.setItem('theme', 'main');
-//         this.#themeSwitcherIcon.innerHTML = this.#themeSwitcherIcons[0];
-//         this.#themeSwitcherGradient.style.fill = 'url(#linear-gradient-1)';
-//         this.#themeSwitcherGradient.style.stroke = 'url(#linear-gradient-1)';
-    
-//         this.#deactivateBtnForTime();
-//     }
-    
-//     #init() {
-//         if (this.#currentTheme === 'light') {
-//             this.#switchToLightMode();
-//             this.#themeSwitcher.removeAttribute("disabled", "");
-//         }
-
-//         this.#themeSwitcher.addEventListener('click', () => {
-//             this.#currentTheme = localStorage.getItem('theme');
-//             this.#currentTheme !== 'light' ? this.#switchToLightMode() : this.#switchToMainMode();
-//         });
-//     }
-
-//     run() {
-//         this.#init();
-//     }
-// }
-
-// const calc = new Calculator();
-
-// calc.run();
