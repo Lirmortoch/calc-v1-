7366adc192806a7bd1f72e8597a894bf5442464c
Lirmortoch <dima2111323@gmail.com>
2024-11-02 16:20:49 +0300

    Немного изменил стили и добавил кнопку для смены режимов

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import {evaluate} from './node_modules/mathjs';
+// import {evaluate} from './node_modules/mathjs';
 
 const body = document.querySelector('body');
 
@@ -13,16 +13,13 @@ const inputMainPart = input.lastElementChild;
 const buttons = body.querySelector('.action-buttons');
 
 const calcMode = 'standard';
-const buttonsMainOps = ['%', 'CE', 'C', '⌫', '⅟x', 'x²', '²√x', '÷', ''];
-const scientificButtons = [];
+const buttonsMainOps = ['%', 'CE', 'C', '⌫', '⅟x', 'x²', '²√x', '÷', 'X', '-', '+'];
+const scientificButtons = [['2','nd'], 'π', 'e', '|x|', 'exp', 'mod', 'n!', ['x', 'y'], ['10', 'x'], 'log', 'ln'];
+const numberButtons = [];
 const scientificTopButtons = ['Trigonometry', 'Functions'];
 
 const history = [];
 const memory = [];
-
-function initCalc() {
-
-}
 
 /* Theme switch */
 const themeSwitcherIcons = ['<circle cx="12" cy="12" r="5"/><line x1="12" x2="12" y1="1" y2="3"/><line x1="12" x2="12" y1="21" y2="23"/><line x1="4.22" x2="5.64" y1="4.22" y2="5.64"/><line x1="18.36" x2="19.78" y1="18.36" y2="19.78"/><line x1="1" x2="3" y1="12" y2="12"/><line x1="21" x2="23" y1="12" y2="12"/><line x1="4.22" x2="5.64" y1="19.78" y2="18.36"/><line x1="18.36" x2="19.78" y1="5.64" y2="4.22"/>', '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>'];
@@ -100,3 +97,56 @@ function turnOnHistoryMode(currentMode) {
 if (currentHistoryMode !== null) turnOnHistoryMode(currentHistoryMode);
 
 historyBlock.addEventListener('click', switchHistoryMode);
+
+function initCalc() {
+    
+}
+
+initCalc();
+
+
+// function initCalc() {
+//     const body = document.querySelector('body');
+
+//     /* Main functionality */
+//     const input = body.querySelector('.input');
+
+//     const inputPrev = input.firstElementChild;
+//     const inputMainPart = input.lastElementChild;
+
+//     const buttons = body.querySelector('.action-buttons');
+
+//     const calcMode = 'standard';
+//     const buttonsMainOps = ['%', 'CE', 'C', '⌫', '⅟x', 'x²', '²√x', '÷', 'X', '-', '+'];
+//     const scientificButtons = [['2','nd'], 'π', 'e', '|x|', 'exp', 'mod', 'n!', ['x', 'y'], ['10', 'x'], 'log', 'ln'];
+//     const numberButtons = [];
+//     const scientificTopButtons = ['Trigonometry', 'Functions'];
+
+//     const history = [];
+//     const memory = [];
+
+//     /* Theme switch */
+//     const themeSwitcherIcons = ['<circle cx="12" cy="12" r="5"/><line x1="12" x2="12" y1="1" y2="3"/><line x1="12" x2="12" y1="21" y2="23"/><line x1="4.22" x2="5.64" y1="4.22" y2="5.64"/><line x1="18.36" x2="19.78" y1="18.36" y2="19.78"/><line x1="1" x2="3" y1="12" y2="12"/><line x1="21" x2="23" y1="12" y2="12"/><line x1="4.22" x2="5.64" y1="19.78" y2="18.36"/><line x1="18.36" x2="19.78" y1="5.64" y2="4.22"/>', '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>'];
+
+//     const themeSwitcher = document.querySelector('.header__theme-switcher');
+//     const themeSwitcherIcon = document.getElementById('switcher-mask');
+//     const themeSwitcherGradient = document.getElementById('switcher-gradient');
+
+//     let currentTheme = localStorage.getItem('theme');
+
+//     if (currentTheme === 'light') {
+//         switchToLightMode();
+//     }
+
+//     themeSwitcher.addEventListener('click', switchTheme);
+    
+//     /* History switch */
+//     const historyBlock = document.querySelector('.history-section');
+//     const historyActivePart = historyBlock.querySelector('.active-part');
+    
+//     let currentHistoryMode = localStorage.getItem('history-mode');
+
+//     if (currentHistoryMode !== null) turnOnHistoryMode(currentHistoryMode);
+
+//     historyBlock.addEventListener('click', switchHistoryMode);
+// }
