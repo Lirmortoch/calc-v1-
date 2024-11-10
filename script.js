f03ce2119ec82bfebebb82fb5864b5d8ab577242
Lirmortoch <dima2111323@gmail.com>
2024-11-10 20:26:00 +0300

    Добавил скрипт для переключения режимов

--- script.js
+++ script.js
@@ -97,3 +97,24 @@ function turnOnHistoryMode(currentMode) {
 if (currentHistoryMode !== null) turnOnHistoryMode(currentHistoryMode);
 
 historyBlock.addEventListener('click', switchHistoryMode);
+
+function keepOpenDropMenu(e) {
+    const isDropDownButton = e.target.matches('[data-dropdown-button]');
+
+    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return;
+
+    let currentDropDown;
+
+    if (isDropDownButton) {
+        currentDropDown = e.target.closest('[data-dropdown]');
+        currentDropDown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropDown => {
+        if (dropDown === currentDropDown) return;
+        dropDown.classList.remove('active');
+    });
+}
+
+body.addEventListener('click', keepOpenDropMenu);
+
